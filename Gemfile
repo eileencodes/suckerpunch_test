531bdff70dcfd9e04c3d4896b7c831274b395f3b
eileencodes <eileencodes@gmail.com>
2016-04-13 07:53:30 -0400

    Add suckerpunch

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.0'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+#
+gem 'sucker_punch'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # queue adapter
+  config.active_job.queue_adapter = :sucker_punch
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
